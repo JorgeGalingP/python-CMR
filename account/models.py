0f41ceb9703969b9b3e335d1021169afe579e212
JorgeGalingP <jorgape225@gmail.com>
2020-05-05 20:07:01 +0200

    Populated templates with basic data from queries
    Updated gitignore to not track migrations, updated templates and schema

--- account/models.py
+++ account/models.py
@@ -3,13 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True) # null=True on a CharField go against the convention
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.email
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -21,10 +28,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tag = models.ManyToManyField(Tag)
 
-    
+    def __str__(self):
+        return self.name
+
+
 class Order(models.Model):
     STATUS = (
         ('Pending', 'Pending'),
@@ -32,5 +43,10 @@ class Order(models.Model):
         ('Delivered', 'Delivered'),
     )
 
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.date_created
--- account/views.py
+++ account/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from .models import Order, Customer, Product
 
 def home(request):
-    return render(request, 'account/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending}
+
+    return render(request, 'account/dashboard.html', context)
 
 def products(request):
-    return render(request, 'account/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'account/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'account/customer.html')
