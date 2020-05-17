ed9637025c7a38a0a2c7d130e7e778b4cbaeab8d
JorgeGalingP <jorgape225@gmail.com>
2020-05-17 18:31:11 +0200

    Added bootstrap styling to register template

--- account/forms.py
+++ account/forms.py
@@ -19,6 +19,20 @@ class OrderForm(ModelForm):
 
 
 class CreateUserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    email=forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), 
+        max_length=64, 
+        help_text='Enter a valid email address.')
+    password1=forms.CharField(
+        label= ('Password'),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        help_text='Your password can’t be too similar to your other personal information, must contain at least 8 characters, can’t be a commonly used password and can’t be entirely numeric.')
+    password2=forms.CharField(
+        label= ('Repeat password'),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        help_text='Enter the same password as before, for verification.')
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
