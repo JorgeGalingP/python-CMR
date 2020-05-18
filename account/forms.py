54476bd4fafb10d43935dcf65d030a6d44afaf3d
JorgeGalingP <jorgape225@gmail.com>
2020-05-18 21:50:26 +0200

    Added bootstrap styles in order and register forms

--- account/forms.py
+++ account/forms.py
@@ -3,9 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-from .models import Order
+from .models import Order, Product
 
 class OrderForm(ModelForm):
+    product = forms.ModelChoiceField(
+                        queryset=Product.objects.all(),
+                        empty_label="Select a product...",
+                        widget=forms.Select(attrs={'class': 'form-control'}))
+    status = forms.ChoiceField(
+                        choices=Order.STATUS, 
+                        widget=forms.Select(attrs={'class': 'form-control'}))
+    note = forms.CharField(
+                        max_length=200,
+                        widget=forms.Textarea(attrs={'class':'form-control'}),
+                        help_text='Maximum 200 characters.')
+
     def __init__(self, *args, **kwargs):
         super(OrderForm, self).__init__(*args, **kwargs)
         self.fields['customer'].disabled = True
@@ -16,22 +28,27 @@ class OrderForm(ModelForm):
         widgets = {
             'customer': HiddenInput(),
         }
+        labels = {
+            'product': 'Product',
+            'status': 'Status',
+            'note': 'Note',
+        }
 
 
 class CreateUserForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     email=forms.EmailField(
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), 
-        max_length=64, 
-        help_text='Enter a valid email address.')
+                        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), 
+                        max_length=64, 
+                        help_text='Enter a valid email address.')
     password1=forms.CharField(
-        label= ('Password'),
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
-        help_text='Your password can’t be too similar to your other personal information, must contain at least 8 characters, can’t be a commonly used password and can’t be entirely numeric.')
+                        label= ('Password'),
+                        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+                        help_text='Your password can’t be too similar to your other personal information, must contain at least 8 characters, can’t be a commonly used password and can’t be entirely numeric.')
     password2=forms.CharField(
-        label= ('Repeat password'),
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
-        help_text='Enter the same password as before, for verification.')
+                        label= ('Repeat password'),
+                        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+                        help_text='Enter the same password as before, for verification.')
 
     class Meta:
         model = User
