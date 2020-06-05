82859613b5c5cd841e25a7ab8e919296fc5b4ca9
JorgeGalingP <jorgape225@gmail.com>
2020-06-05 10:52:34 +0200

    Updated database to AWS postgress

--- cmr/settings.py
+++ cmr/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'cmr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'a_name',
+        'USER': 'a_user',
+        'PASSWORD': 'a_password',
+        'HOST': 'a_host',
+        'PORT': '5432'
     }
 }
 
